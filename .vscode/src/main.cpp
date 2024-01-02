e78fcf10c7a96002619805727fb9929e8653fb8d
joeyissa <joeyissa59@gmail.com>
2024-01-02 01:49:30 -0500

    fixed compiler issues

---
+++ .vscode/src/main.cpp
@@ -0,0 +1,31 @@
+#include <raylib.h>
+
+int main() {
+    // Determin the Game Window Width and Height
+    const int screenWidth = 300;
+    const int screenHeight = 600;
+
+    Color screenColor = Color{24, 20, 211, 255};
+
+    // Initialize the Window
+    InitWindow(screenWidth, screenHeight, "Tetris");
+
+    // Setting the Frames Per Second
+    SetTargetFPS(60);
+
+    // The Game Loop
+    while (!WindowShouldClose() /*WindowShouldClose returns true if esc is clicked and closes the window*/) {
+
+        // Setup Canvas
+        BeginDrawing();
+        // Clear canvas to a specific color to avoid flicker
+        ClearBackground(screenColor);
+
+        // Here goes all the Game Logic
+
+        // teardown Canvas
+        EndDrawing();
+    }
+    CloseWindow();
+    return 0;
+}
